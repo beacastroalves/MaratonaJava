3dfb1582a2572e065a51429320ee74e8e9284965
beatriz alves <beacastro1008@gmail.com>
2023-11-09 22:34:36 +0000

    feat: add a new exercise

---
+++ src/academy/devdojo/introductiontoprogramming/ExerciseSalaryTax.java
@@ -0,0 +1,11 @@
+package academy.devdojo.introductiontoprogramming;
+
+public class ExerciseSalaryTax {
+  public static void main(String[] args) {
+    float salary = 5450.89F;
+    float taxPercentage = 30;
+    float taxAmountFromSalary = salary * (taxPercentage / 100);
+    System.out.println("The amount is: " + taxAmountFromSalary);
+
+  }
+}
