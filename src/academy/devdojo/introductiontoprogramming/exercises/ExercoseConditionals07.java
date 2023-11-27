629d66e4c7b78fc198e68bdb1e4ba1ce65624325
beatriz alves <beacastro1008@gmail.com>
2023-11-27 23:13:20 +0000

    feat: add a new exercise, switch case

---
+++ src/academy/devdojo/introductiontoprogramming/exercises/ExercoseConditionals07.java
@@ -0,0 +1,30 @@
+package academy.devdojo.introductiontoprogramming.exercises;
+
+import java.util.Scanner;
+
+public class ExercoseConditionals07 {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    System.out.println("Type a number for the day of the week between 1 and 7. Use 1 for Sunday.");
+    int day = input.nextInt();
+
+    switch (day) {
+      case 2:
+      case 3:
+      case 4:
+      case 5:
+      case 6:
+        System.out.println("Weekday");
+        break;
+      case 1:
+      case 7:
+        System.out.println("Weekend");
+        break;
+      default:
+        System.out.println("Invalid input");
+        break;
+
+
+    }
+  }
+}
