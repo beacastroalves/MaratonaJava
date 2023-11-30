108212291c2d19e5b14f0868a0110e831eee2c12
beatriz alves <beacastro1008@gmail.com>
2023-11-30 17:08:52 +0000

    feat: new loop lesson, for

---
+++ src/academy/devdojo/introductiontoprogramming/loops/fors/For01.java
@@ -0,0 +1,9 @@
+package academy.devdojo.introductiontoprogramming.loops.fors;
+
+public class For01 {
+  public static void main(String[] args) {
+    for(int counter = 0; counter < 10; counter++) {
+      System.out.println("Counter: " + counter);
+    }
+  }
+}
