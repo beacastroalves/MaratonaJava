b50cf93237580220f16d3fa3fcf99e04ed6ce2f8
beatriz alves <beacastro1008@gmail.com>
2023-11-30 17:59:10 +0000

    feat: add new exercises, loops

--- src/academy/devdojo/introductiontoprogramming/exercises/ExerciseFor02.java
+++ src/academy/devdojo/introductiontoprogramming/exercises/ExerciseFor02.java
@@ -4,7 +4,6 @@ public class ExerciseFor02 {
   public static void main(String[] args) {
     for (int i = 1; i <= 12; i++) {
       System.out.println(i + " x 5 = " + (i*5));
-
     }
   }
 }
---
+++ src/academy/devdojo/introductiontoprogramming/exercises/ExerciseFor04Calculator.java
@@ -0,0 +1,66 @@
+package academy.devdojo.introductiontoprogramming.exercises;
+
+import java.util.Scanner;
+
+public class ExerciseFor04Calculator {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    int n1 = 0;
+    int n2 = 0;
+    int optionMath = 0;
+    boolean menu = true;
+    do {
+      System.out.println("Choose the math option\n" +
+          "1 - Sum\n" +
+          "2 - Subtraction\n" +
+          "3 - Multiplication\n" +
+          "4 - Division\n" +
+          "0 - Exit");
+
+      optionMath = 1;
+      optionMath = input.nextInt();
+      switch (optionMath) {
+        case 1:
+          System.out.println("Sum, ok. Type the first number");
+          n1 = input.nextInt();
+          System.out.println("Type the second number");
+          n2 = input.nextInt();
+          System.out.println(n1 + " + " + n2 + " = " + (n1 + n2));
+          System.out.println("------------------------------");
+          break;
+        case 2:
+          System.out.println("Subtraction, ok. Type the first number");
+          n1 = input.nextInt();
+          System.out.println("Type the second number");
+          n2 = input.nextInt();
+          System.out.println(n1 + " - " + n2 + " = " + (n1 - n2));
+          System.out.println("------------------------------");
+          break;
+        case 3:
+          System.out.println("Multiplication, ok. Type the first number");
+          n1 = input.nextInt();
+          System.out.println("Type the second number");
+          n2 = input.nextInt();
+          System.out.println(n1 + " x " + n2 + " = " + (n1 * n2));
+          System.out.println("------------------------------");
+          break;
+        case 4:
+          System.out.println("Division, ok. Type the first number");
+          float d1 = input.nextInt();
+          System.out.println("Type the second number");
+          float d2 = input.nextInt();
+          System.out.println(d1 + " / " + d2 + " = " + (d1 / d2));
+          System.out.println("------------------------------");
+          break;
+        default:
+          if (optionMath != 0) {
+            System.out.println("Option invalid. Try again");
+            System.out.println("------------------------------");
+          }
+      }
+    }
+    while (optionMath != 0);
+
+
+  }
+}
