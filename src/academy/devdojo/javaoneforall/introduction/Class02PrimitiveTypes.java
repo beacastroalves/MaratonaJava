db32ba68dd5993cbc153b98615bcc0b4d27525da
beatriz alves <beacastro1008@gmail.com>
2024-01-02 11:29:03 +0000

    12 Primitive types pt 03 - casting

--- src/academy/devdojo/javaoneforall/introduction/Class02PrimitiveTypes.java
+++ src/academy/devdojo/javaoneforall/introduction/Class02PrimitiveTypes.java
@@ -4,6 +4,25 @@ public class Class02PrimitiveTypes {
   public static void main(String[] args) {
     // int, double, float, boolean, char, byte, short, long
     int age = 10;
-    System.out.println("The age is " + age + " years old");
+    long bigNumber = (long) 155.35;
+    float salaryFloat1 = 2500.00F;
+    float salaryFloat2 = (float) 2500.00D;
+    double salaryDouble = 2500;
+    boolean trueValue = true;
+    boolean falseValue = false;
+    short shortNumber = 32000;
+    byte byteNumber = 127;
+    char character = '\u0041';
+
+    System.out.println("The age is: " + age + " years old");
+    System.out.println("bigNumber: " + bigNumber);
+    System.out.println("salaryFloat1: " + salaryFloat1);
+    System.out.println("salaryFloat2: " + salaryFloat2);
+    System.out.println("salaryDouble: " + salaryDouble);
+    System.out.println("trueValue: " + trueValue);
+    System.out.println("falseValue: " + falseValue);
+    System.out.println("shortNumber: " + shortNumber);
+    System.out.println("byteNumber: " + byteNumber);
+    System.out.println("character: " + character);
   }
 }
