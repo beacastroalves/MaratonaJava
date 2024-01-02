6f97648c9df1c7836916b48fdaee53dbca8707b2
beatriz alves <beacastro1008@gmail.com>
2024-01-02 11:04:22 +0000

    Java One For All - 09 - Comments

---
+++ src/academy/devdojo/javaoneforall/introduction/Class01HelloDevDojo.java
@@ -0,0 +1,11 @@
+package academy.devdojo.javaoneforall.introduction;
+/*
+* This is the class HelloDevDojo and this is <b>javadoc</b>
+* */
+public class Class01HelloDevDojo {
+  // I can type anything and it will be completely ignored
+
+  public static void main(String[] args) {
+    System.out.println("KA-ME-HA-ME-HAAA");
+  }
+}
