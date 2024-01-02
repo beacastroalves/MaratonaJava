ee7af3790449823fa2f77fec7ce202d800d56ba1
beatriz alves <beacastro1008@gmail.com>
2024-01-02 12:23:02 +0000

    15 - Operators pt 01 - Arithmetic

---
+++ src/academy/devdojo/javaoneforall/introduction/Class04Operators.java
@@ -0,0 +1,12 @@
+package academy.devdojo.javaoneforall.introduction;
+
+public class Class04Operators {
+  public static void main(String[] args) {
+    // + - / *
+    int number01 = 10;
+    int number02 = 20;
+    double result = number01 / (double) number02;
+
+    System.out.println(result);
+  }
+}
