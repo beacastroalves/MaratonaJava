66f4e078097f2d6616e50b66fc0d19f29ec80138
beatriz alves <beacastro1008@gmail.com>
2024-01-03 11:49:20 +0000

    refactor: 16 - Operators pt 02 - Relational

--- src/academy/devdojo/javaoneforall/introduction/Class04Operators.java
+++ src/academy/devdojo/javaoneforall/introduction/Class04Operators.java
@@ -8,5 +8,25 @@ public class Class04Operators {
     double result = number01 / (double) number02;
 
     System.out.println(result);
+
+    // %
+    int modulo = 21 % 7;
+    System.out.println(modulo);
+
+    // < > <= >= != ==
+    boolean isTenLowerThanTwenty = 10 < 20;
+    boolean isTenGreaterThanTwenty = 20 <= 10;
+    boolean isTenLowerOrEqualsThanTwenty = 10 < 20;
+    boolean isTenGreaterOrEqualsThanTwenty = 20 <= 10;
+    boolean isTenDifferentThanTwenty = 20 != 10;
+    boolean isTenEqualsTwenty = 20 == 10;
+    System.out.println("isTenLowerThanTwenty " + isTenLowerThanTwenty);
+    System.out.println("isTenGreaterThanTwenty " + isTenGreaterThanTwenty);
+    System.out.println("isTenLowerOrEqualsThanTwenty " + isTenLowerOrEqualsThanTwenty);
+    System.out.println("isTenGreaterOrEqualsThanTwenty " + isTenGreaterOrEqualsThanTwenty);
+    System.out.println("isTenDifferentThanTwenty " + isTenDifferentThanTwenty);
+    System.out.println("isTenEqualsTwenty " + isTenEqualsTwenty);
+
+
   }
 }
