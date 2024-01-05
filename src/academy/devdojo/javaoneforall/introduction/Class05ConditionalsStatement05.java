77d148ce88b9fa2d1337113c39479873fee543f5
beatriz alves <beacastro1008@gmail.com>
2024-01-05 17:29:24 +0000

    25 - Conditional Statement pt 06 - Switch

---
+++ src/academy/devdojo/javaoneforall/introduction/Class05ConditionalsStatement05.java
@@ -0,0 +1,45 @@
+package academy.devdojo.javaoneforall.introduction;
+
+public class Class05ConditionalsStatement05 {
+  public static void main(String[] args) {
+    byte day = 8;
+    // char, int, byte, short, enum, String
+    switch (day) {
+      case 1:
+        System.out.println("Sunday");
+        break;
+      case 2:
+        System.out.println("Monday");
+        break;
+      case 3:
+        System.out.println("Tuesday");
+        break;
+      case 4:
+        System.out.println("Wednesday");
+        break;
+      case 5:
+        System.out.println("Thursday");
+        break;
+      case 6:
+        System.out.println("Friday");
+        break;
+      case 7:
+        System.out.println("Invalid");
+        break;
+      default:
+        break;
+    }
+    char gender = 'M';
+    switch (gender) {
+      case 'M':
+        System.out.println("Male");
+        break;
+      case 'F':
+        System.out.println("Female");
+        break;
+      default:
+        System.out.println("Invalid Option");
+        break;
+    }
+  }
+}
