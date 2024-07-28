ebf5a726ef22025672ead8ce0c67c83e1ef4b7ae
beatriz alves <beacastro1008@gmail.com>
2024-07-28 19:34:03 +0100

    72 - Object Orientation - Inheritance pt2 - super

--- src/academy/devdojo/javaoneforall/javacore/h/inheritance/domain/Manager.java
+++ src/academy/devdojo/javaoneforall/javacore/h/inheritance/domain/Manager.java
@@ -3,7 +3,11 @@ package academy.devdojo.javaoneforall.javacore.h.inheritance.domain;
 public class Manager extends Employee {
   private String department;
 
-
+  @Override
+  public void print() {
+    super.print();
+    System.out.println(this.department);
+  }
 
 
   public String getDepartment() {
