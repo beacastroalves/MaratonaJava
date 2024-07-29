1b1d3f1b7ead47e0b09b3971a111753fc699c3cb
beatriz alves <beacastro1008@gmail.com>
2024-07-29 16:05:45 +0100

    73 - OO - Inheritance pt 03 - protected

--- src/academy/devdojo/javaoneforall/javacore/h/inheritance/domain/Manager.java
+++ src/academy/devdojo/javaoneforall/javacore/h/inheritance/domain/Manager.java
@@ -7,8 +7,13 @@ public class Manager extends Employee {
   public void print() {
     super.print();
     System.out.println(this.department);
+    playmentReport();
   }
 
+  public void playmentReport() {
+    System.out.println("The employee " + this.name + " from the department " + this.department +
+        " received a salary of " + this.salary);
+  }
 
   public String getDepartment() {
     return department;
