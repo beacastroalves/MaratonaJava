a52dff9398fe0a1d8a812ead20a2ea607bb674a5
beatriz alves <beacastro1008@gmail.com>
2024-07-30 14:18:43 +0100

    77 - OO - Final Modifier pt 01 - Primitive types

---
+++ src/academy/devdojo/javaoneforall/javacore/j/finalmodifier/domain/Car.java
@@ -0,0 +1,45 @@
+package academy.devdojo.javaoneforall.javacore.j.finalmodifier.domain;
+
+public class Car {
+  private String name;
+  public final double SPEED_LIMIT;
+  /*
+  OPTION 1 - With 'static final' on attribute and static init block
+  #0 Static init block from the superclass is executed when JVM loads the class and only once
+
+  public static final double SPEED_LIMIT;
+
+  static {
+    SPEED_LIMIT = 250;
+  }
+
+  */
+
+  /*
+  OPTION 2 - Without 'static final' on attribute and static init block
+  #4 - Init block from the superclass is executed in the order they appear
+
+  public final double SPEED_LIMIT;
+
+  {
+    SPEED_LIMIT = 250;
+  }
+
+  */
+
+  /*
+  OPTION 3 - Initialize the attribute inside constructor
+  #5 - The constructor from the superclass is executed
+  */
+  public Car() {
+    SPEED_LIMIT = 250;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+}
---
+++ src/academy/devdojo/javaoneforall/javacore/j/finalmodifier/test/CarTest01.java
@@ -0,0 +1,7 @@
+package academy.devdojo.javaoneforall.javacore.j.finalmodifier.test;
+
+public class CarTest01 {
+  public static void main(String[] args) {
+    
+  }
+}
